27881db4f35fd3568b78f3e35ef62036028ea98d
EduardoRibeiroGodoi <147395041+EduardoRibeiroGodoi@users.noreply.github.com>
2024-10-31 12:06:38 -0300

    j

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 function criarCartao(pergunta,resposta){
   let conteudo=document.getElementById("conteudo")
-console.log(conteudo)
+let Cartao=document.createElement("article")
+Cartao.classList='cartao'
 
+Cartao.innerHTML=`
+<div class="cartao--conteudo">
+                <div class="cartao--pergunta"><p>1.PRINCIPAIS TITULOS DO CORINTHIANS </div></p>
+                <div class="cartao--resposta"><p>-2 MUNDIAIS DE CLUBES 1 LIBERTADORES</div></p>
+                </div>
+`
+conteudo.appendChild(Cartao)
 }
 
 criarCartao(1,2)
