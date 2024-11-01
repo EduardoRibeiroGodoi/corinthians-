dbcaa095c9b30aa797e98948e7a896728c024a1b
EduardoRibeiroGodoi <147395041+EduardoRibeiroGodoi@users.noreply.github.com>
2024-11-01 08:38:50 -0300

    t

---
+++ pergunta.js
@@ -0,0 +1,24 @@
+criarCartao(
+    "PRINCIPAIS TITULOS DO CORINTHIANS ",
+    " MUNDIAIS DE CLUBES 1 LIBERTADORES"
+)
+criarCartao(
+    "QUAL E 0 PRINCIPAL APELIDO DO CORINTHIANS",
+    "TIMAO "
+)
+criarCartao(
+    "QUAL FOI O PRIMEIRO MUNDIAL DE CLUBES DO CORINTHIANS",
+    " FOI NO ANO 2000"
+)
+criarCartao(
+    "QUANTOS TITULOS O CORINTHIANS TEM",
+    " 54 troféus ao longo de sua história"
+)
+criarCartao(
+    "QUEM E O MAIOR JOGADOR DA HISTORIA DO CORINTHIANS  ",
+    " Socrates"
+)
+criarCartao(
+    "QUANDO O CORINTHIANS FOI FUNDADO  ",
+    " 1910"
+)
