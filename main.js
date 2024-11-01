fee6a74a6c2bf73dc76765022a5af18a3be9a8d9
EduardoRibeiroGodoi <147395041+EduardoRibeiroGodoi@users.noreply.github.com>
2024-11-01 08:24:38 -0300

    kl

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ Cartao.classList='cartao'
 
 Cartao.innerHTML=`
 <div class="cartao--conteudo">
-                <div class="cartao--pergunta"><p>1.PRINCIPAIS TITULOS DO CORINTHIANS </div></p>
-                <div class="cartao--resposta"><p>-2 MUNDIAIS DE CLUBES 1 LIBERTADORES</div></p>
+                <div class="cartao--pergunta"><p>${pergunta} </div></p>
+                <div class="cartao--resposta"><p>${resposta}</div></p>
                 </div>
 `
 conteudo.appendChild(Cartao)
